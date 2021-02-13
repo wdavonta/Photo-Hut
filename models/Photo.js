27e2e75a057c2ddb899ea77fcea50a97e987ebd4
Azambik <71612017+Azambik@users.noreply.github.com>
2021-02-12 19:24:44 -0500

    added rough table modeles for database

---
+++ models/Photo.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Photo extends Model {
+  static upvote(body, models) {
+    return models.Vote.create({
+      user_id: body.user_id,
+      Photo_id: body.photo_id
+    }).then(() => {
+      return Photo.findOne({
+        where: {
+          id: body.photo_id
+        },
+        attributes: [
+          'id',
+          'photo_url',
+          'title',
+          'created_at',
+          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE photo.id = vote.photo_id)'), 'vote_count']
+        ],
+        include: [
+          {
+            model: models.Comment,
+            attributes: ['id', 'comment_text', 'photo_id', 'user_id', 'created_at'],
+            include: {
+              model: models.User,
+              attributes: ['username']
+            }
+          }
+        ]
+      });
+    });
+  }
+}
+
+
+Photo.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    photo_url: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
+    }
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'photo'
+  }
+);
+
+module.exports = Photo;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,69 @@
-console.log("hello world!");
+// import all models
+const Photo = require('./Photo');
+const User = require('./User');
+const Vote = require('./Vote');
+const Comment = require('./Comment');
+
+// create associations
+User.hasMany(Photo, {
+  foreignKey: 'user_id'
+});
+
+Photo.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+User.belongsToMany(Photo, {
+  through: Vote,
+  as: 'voted_photos',
+
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Photo.belongsToMany(User, {
+  through: Vote,
+  as: 'voted_photos',
+  foreignKey: 'photo_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Vote.belongsTo(Photo, {
+  foreignKey: 'photo_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Vote, {
+  foreignKey: 'user_id'
+});
+
+Photo.hasMany(Vote, {
+  foreignKey: 'photo_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Comment.belongsTo(Photo, {
+  foreignKey: 'photo_id',
+  onDelete: 'SET NULL'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'SET NULL'
+});
+
+Photo.hasMany(Comment, {
+  foreignKey: 'photo_id'
+});
+
+module.exports = { User, Photo, Vote, Comment };
