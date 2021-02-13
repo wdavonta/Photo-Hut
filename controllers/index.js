2537cb6212ff4c8d428dd966f188528f9287ee1b
dcharney <delaneycharney@gmail.com>
2021-02-13 16:02:11 +0100

    switching branches since work has become primarily routes

--- controllers/index.js
+++ controllers/index.js
@@ -1,1 +1,15 @@
-console.log("hello world!");
+const router = require('express').Router();
+
+const homeRoutes = require('./home-routes.js');
+const apiRoutes = require('./api');
+const profileRoutes = require('./profile-routes.js');
+
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
+
+router.use((req, res) => {
+  res.status(404).end();
+});
+
+module.exports = router;
