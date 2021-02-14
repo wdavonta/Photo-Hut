059dc760791aa320ea472d7f5296ae351d079854
Da'Vonta Williamson <69822943+wdavonta@users.noreply.github.com>
2021-02-13 22:55:11 -0500

    added seeds for user for username and password

---
+++ seeds/user-seeds.js
@@ -0,0 +1,21 @@
+const sequelize = require('../config/connection');
+const { User, Post } = require('../models');
+
+const userdata = [
+  {
+    username: 'alesmonde0',
+    password: 'password123'
+  },
+  {
+    username: 'jwilloughway1',
+    password: 'password123'
+  },
+  {
+    username: 'iboddam2',
+    password: 'password123'
+  },
+];
+
+  const seedUsers = () => User.bulkCreate(userdata, {individualHooks: true});
+
+module.exports = seedUsers;
