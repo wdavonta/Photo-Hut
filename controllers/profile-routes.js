70646d874fb51623be664ed5c2ca49fcc80e102f
dcharney <delaneycharney@gmail.com>
2021-02-17 00:45:48 +0100

    modified media queries

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -48,4 +48,54 @@ router.get('/', withAuth, (req, res) => {
     });
 });
 
+// load a different users profile
+router.get('/:id', withAuth, (req, res) => {
+    console.log('==========USER PROFILE============');
+    Photo.findAll({
+        where: {
+            user_id: req.params.id
+        },
+        attributes: [
+            'id',
+            'photo_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE photo.id = vote.photo_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'photo_id', 'user_id', 'created_at'],
+                include: {
+                model: User,
+                attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPhotoData => {
+        if (!dbPhotoData) {
+            res.status(404).json({ message: 'No photo found with this id' });
+            return;
+        }
+
+        const photos = dbPhotoData.map(photo => photo.get({ plain: true }));
+        const username = photos[1].user.username;
+        
+        res.render('profile', {
+            photos,
+            loggedIn: true,
+            username
+        });
+    })
+    .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+    });
+});
+
 module.exports = router;
