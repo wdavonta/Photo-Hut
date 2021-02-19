285b80ffe97d01daa159d6338909a6b66723f6cd
Azambik <71612017+Azambik@users.noreply.github.com>
2021-02-19 16:04:05 -0500

    fixed a small bug in controler/index.js

--- controllers/index.js
+++ controllers/index.js
@@ -5,12 +5,10 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const profileRoutes = require('./profile-routes.js');
-const updateRoutes = require('./update-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/profile', profileRoutes);
-router.use('/update',updateRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
